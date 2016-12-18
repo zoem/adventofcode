069f027a2f59a71e61873b1ffbb3685fb23f8730
zoem <zoem@fok.nl>
2016-12-18 19:02:26 +0100

    day 18 minor improvement

--- 2016/day18.php
+++ 2016/day18.php
@@ -4,19 +4,22 @@ $input = trim(file_get_contents('day18.txt'));
 
 function countTiles($input, $rows) {
     $input = '1' . strtr($input, ['^' => 0, '.' => 1]) . '1';
+    $input = str_split($input);
 
-    $sum = substr_count($input, 1) - 2;
+    $sum = array_sum($input);
+    $max = count($input)-1;
     for ($j=1; $j<$rows; $j++) {
-        $row = '1';
-        for ($i=1; $i<strlen($input)-1; $i++) {
-            $x = 2 * $input[$i-1] + $input[$i+1];
-            $row .= $x == 1 || $x == 2 ? 0 : 1;
+        $row = $input;
+
+        for ($i=1; $i<$max; $i++) {
+            $row[$i] = $input[$i-1] != $input[$i+1] ? 0 : 1;
         }
-        $row .= '1';
+
         $input = $row;
-        $sum += substr_count($input, 1) - 2;
+        $sum += array_sum($input);
     }
-    return $sum;
+
+    return $sum  - $rows * 2;
 }
 
 printf('ans#18.1: %s'.PHP_EOL, countTiles($input, 40));
