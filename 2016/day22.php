b48aea02d1e7dc68426395c5f3a37939361a9bed
zoem <zoem@fok.nl>
2016-12-22 11:48:04 +0100

    day 22

---
+++ 2016/day22.php
@@ -0,0 +1,38 @@
+<?php
+
+$input = file_get_contents('day22.txt');
+$lines = array_filter(array_map('trim', explode(PHP_EOL, $input)));
+
+$nodes = [];
+$empty = null;
+$maxX  = 0;
+$dummy = range(1, 10);
+
+foreach ($lines as $line) {
+    if (preg_match('#x(?<x>\d+)-y(?<y>\d+)\s+(?<s>\d+)T\s+(?<u>\d+)T\s+(?<a>\d+)T\s+(?<p>\d+)%#', $line, $match)) {
+        $match = array_diff_key($match, $dummy);
+        $nodes[] = $match;
+        
+        $maxX = max($match['x'], $maxX);
+        
+        if ($match['u'] == 0) {
+            $empty = $match;
+        }
+    }
+}
+
+$sum = 0;
+foreach ($nodes as $a) {
+    foreach ($nodes as $b) {
+        if ($a['x'] == $b['x'] && $a['y'] == $b['y']) {
+            continue;
+        }
+        
+        if ($a['a'] >= $b['u'] && $b['u'] > 0) {
+            $sum++;
+        }
+    }
+}
+
+printf('ans#22.1: %s'.PHP_EOL, $sum);
+printf('ans#22.2: %s'.PHP_EOL, $empty['x'] + $empty['y'] + $maxX + ($maxX - 1) * 5);
